dc98904f8ad51567874fe362cf877266b1838e61
ndmn <pad1_st@bk.ru>
2019-07-07 23:33:55 +0300

    Changed 1 site.

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '2_3wyskd2ulc-2s(4d-mjtjd2_kqg+vh+t59#62g)9d1l+f1zi'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', '<admin>.pythonanywhere.com']
+ALLOWED_HOSTS = ['127.0.0.1', 'pad1.pythonanywhere.com']
 
 
 # Application definition
